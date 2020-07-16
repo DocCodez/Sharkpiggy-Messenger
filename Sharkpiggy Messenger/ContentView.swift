a92e0c7e5dff163161b6a60897799d4ef2c247fc
DocCodez <adielhernandez15@gmail.com>
2020-07-16 02:53:51 -0400

    Added all starter files

---
+++ Sharkpiggy Messenger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sharkpiggy Messenger
+//
+//  Created by Adiel Hernandez on 7/16/20.
+//  Copyright © 2020 Adiel Hernandez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
