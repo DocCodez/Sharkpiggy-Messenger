a3d6f43bb18f24c6e5bec3f1862c4c8693dc6945
DocCodez <adielhernandez15@gmail.com>
2020-07-17 15:49:48 -0400

    Working basic messenger

--- Sharkpiggy Messenger/ContentView.swift
+++ Sharkpiggy Messenger/ContentView.swift
@@ -7,10 +7,44 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct ContentView: View {
+    
+    @State var status = UserDefaults.standard.value(forKey: "status") as? Bool ?? false
+    
     var body: some View {
-        Text("Hello, World!")
+       
+        VStack{
+            
+            if status{
+
+                NavigationView{
+                    
+                  Home()
+                    .environmentObject(MainObservable())
+                }
+                
+            }
+            else{
+
+                NavigationView{
+
+                     FirstPage()
+                }
+            }
+            
+            
+        }.onAppear {
+            
+            NotificationCenter.default.addObserver(forName: NSNotification.Name("statusChange"), object: nil, queue: .main) { (_) in
+                
+               let status = UserDefaults.standard.value(forKey: "status") as? Bool ?? false
+                   
+                self.status = status
+            }
+        }
+       
     }
 }
 
@@ -18,4 +52,92 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+// Refer the previous Video For Setting Up OTP Verification And Registration Page in SwiftUI
+// Link In The Description...
+
+class MainObservable : ObservableObject{
+    
+    @Published var recents = [Recent]()
+    @Published var norecetns = false
+    
+    init() {
+        
+        let db = Firestore.firestore()
+        let uid = Auth.auth().currentUser?.uid
+        
+        db.collection("users").document(uid!).collection("recents").order(by: "date", descending: true).addSnapshotListener { (snap, err) in
+            
+            if err != nil{
+                
+                print((err?.localizedDescription)!)
+                self.norecetns = true
+                return
+            }
+            
+            if snap!.isEmpty{
+                
+                self.norecetns = true
+            }
+            
+            for i in snap!.documentChanges{
+                
+                if i.type == .added{
+                    
+                    let id = i.document.documentID
+                    let name = i.document.get("name") as! String
+                    let pic = i.document.get("pic") as! String
+                    let lastmsg = i.document.get("lastmsg") as! String
+                    let stamp = i.document.get("date") as! Timestamp
+                    
+                    let formatter = DateFormatter()
+                    formatter.dateFormat = "dd/MM/yy"
+                    let date = formatter.string(from: stamp.dateValue())
+                    
+                    formatter.dateFormat = "hh:mm a"
+                    let time = formatter.string(from: stamp.dateValue())
+                    
+                    self.recents.append(Recent(id: id, name: name, pic: pic, lastmsg: lastmsg, time: time, date: date, stamp: stamp.dateValue()))
+                }
+                
+                if i.type == .modified{
+                    
+                    let id = i.document.documentID
+                    let lastmsg = i.document.get("lastmsg") as! String
+                    let stamp = i.document.get("date") as! Timestamp
+                    
+                    let formatter = DateFormatter()
+                    formatter.dateFormat = "dd/MM/yy"
+                    let date = formatter.string(from: stamp.dateValue())
+                    
+                    formatter.dateFormat = "hh:mm a"
+                    let time = formatter.string(from: stamp.dateValue())
+                    
+                    
+                    for j in 0..<self.recents.count{
+                        
+                        if self.recents[j].id == id{
+                            
+                            self.recents[j].lastmsg = lastmsg
+                            self.recents[j].time = time
+                            self.recents[j].date = date
+                            self.recents[j].stamp = stamp.dateValue()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct Recent : Identifiable {
+    
+    var id : String
+    var name : String
+    var pic : String
+    var lastmsg : String
+    var time : String
+    var date : String
+    var stamp : Date
 }
--- Sharkpiggy Messenger/SceneDelegate.swift
+++ Sharkpiggy Messenger/SceneDelegate.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 import SwiftUI
+import Firebase
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    
+    
+    //Add this to Scene Delegate
 
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        for urlContext in URLContexts {
+            let url = urlContext.url
+            Auth.auth().canHandle(url)
+        }
+      }
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
